7a8271f53a0abe2012f44a3f373fee6b86178403
sgvallala <sgvallala@gmail.com>
2022-05-02 10:43:00 -0400

    INS-43

--- src/bento/globalStatsData.js
+++ src/bento/globalStatsData.js
@@ -7,6 +7,11 @@ export const statsStyling = {
     height: '47px',
     background: '#8DCAFF',
   },
+  statsIcon: {
+    width: '40px',
+    height: '45px',
+    margin: '-7px',
+  },
 };
 
 export const globalStatsData = [
@@ -15,31 +20,43 @@ export const globalStatsData = [
     statTitle: 'Programs',
     type: 'field',
     statAPI: 'numberOfPrograms',
+    statIconSrc: 'https://raw.githubusercontent.com/CBIIT/datacommons-assets/master/icdc/images/svgs/Programs_.svg',
+    statIconAlt: 'Temp',
   },
   {
     statTitle: 'Projects',
     type: 'field',
     statAPI: 'numberOfProjects',
+    statIconSrc: 'https://raw.githubusercontent.com/CBIIT/datacommons-assets/master/icdc/images/svgs/Studies_.svg',
+    statIconAlt: 'Data Volume Stats Bar Icon',
   },
   {
     statTitle: 'Publications',
     type: 'field',
     statAPI: 'numberOfPublicationsByProjects',
+    statIconSrc: 'https://raw.githubusercontent.com/CBIIT/datacommons-assets/master/icdc/images/svgs/Cases_.svg',
+    statIconAlt: 'Data Volume Stats Bar Icon',
   },
   {
     statTitle: 'Datasets',
     type: 'field',
     statAPI: 'numberOfDatasetsByProjects',
+    statIconSrc: 'https://raw.githubusercontent.com/CBIIT/datacommons-assets/master/icdc/images/svgs/Samples_.svg',
+    statIconAlt: 'Data Volume Stats Bar Icon',
   },
   {
     statTitle: 'Clinical Trials',
     type: 'field',
     statAPI: 'numberOfClinicalTrialsByProjects',
+    statIconSrc: 'https://raw.githubusercontent.com/CBIIT/datacommons-assets/master/icdc/images/svgs/CaseFiles_.svg',
+    statIconAlt: 'Data Volume Stats Bar Icon',
   },
   {
     statTitle: 'Patents',
     type: 'field',
     statAPI: 'numberOfPatentsByProjects',
+    statIconSrc: 'https://raw.githubusercontent.com/CBIIT/datacommons-assets/master/icdc/images/svgs/StudyFiles_.svg',
+    statIconAlt: 'Data Volume Stats Bar Icon',
   },
 ];
 
