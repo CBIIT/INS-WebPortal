7ee3fa2e9a587c02b84c7e14a3f8b0ecf6ff3390
sgvallala <sgvallala@gmail.com>
2022-07-01 14:34:50 -0400

    INS-85

--- src/bento/dashboardData.js
+++ src/bento/dashboardData.js
@@ -114,9 +114,7 @@ export const defaultSearch = {
 };
 
 // --------------- Dashboard ActiveFiltersQuery configuration --------------
-export const displayActiveFiltersQuery = false;
-// TO_DO: fix error
-// export const displayActiveFiltersQuery = true;
+export const displayActiveFiltersQuery = true;
 
 // --------------- Dashboard Widgets configuration --------------
 // A maximum of 6 widgets are allowed
