b7a1ec555d3d030631309ebdf4c61470d423b8e9
sgvallala <sgvallala@gmail.com>
2022-08-18 17:18:42 -0400

    INS-249

--- src/pages/about/aboutBody.js
+++ src/pages/about/aboutBody.js
@@ -1,6 +1,7 @@
 /* eslint-disable max-len */
 import React from 'react';
 import aboutImage from '../../assets/about/About.png';
+import flowChart from '../../assets/about/INS-Workflow.svg';
 import './aboutBody.css';
 
 const AboutBody = () => {
