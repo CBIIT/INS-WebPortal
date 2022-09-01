162e12a14f0cb6d11c244c775c1dcd814fd62965
sgvallala <sgvallala@gmail.com>
2022-09-01 15:54:40 -0400

    INS-305

--- src/components/Header/HeaderView.js
+++ src/components/Header/HeaderView.js
@@ -4,7 +4,15 @@ import { withRouter } from 'react-router-dom';
 import headerData from '../../bento/globalHeaderData';
 import SearchAUtoFill from '../Search/searchAutoFillComponent';
 
-const ICDCHeader = (props) => {
+const styles = {
+  nihLogoImg: {
+    minHeight: '100px',
+    cursor: 'pointer',
+    marginLeft: '15px',
+  },
+};
+
+const INSHeader = (props) => {
   const { location } = props;
   return location.pathname.match('/search') ? (
     <Header
@@ -19,9 +27,10 @@ const ICDCHeader = (props) => {
       easter={headerData.globalHeaderImage}
       alt={headerData.globalHeaderLogoAltText}
       homeLink={headerData.globalHeaderLogoLink}
+      customStyle={styles}
       SearchComponent={SearchAUtoFill}
     />
   );
 };
 
-export default withRouter(ICDCHeader);
+export default withRouter(INSHeader);
