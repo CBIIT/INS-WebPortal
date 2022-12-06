4fa286e936dac0e7fa6528581644e6b6a03ec187
David-YuWei <ywwei85@gmail.com>
2022-12-05 19:17:25 -0500

    Update INS-86 feature: remove unused code to improve page loading time

--- src/components/Footer/FooterView.js
+++ src/components/Footer/FooterView.js
@@ -14,6 +14,7 @@ const styles = {
 const INSFooter = () => {
   const [footerUpdatedData, setFooterUpdatedData] = useState(FooterData);
 
+  /*
   useEffect(() => {
     const getSystems = async () => {
       const response = await fetch(
@@ -25,6 +26,7 @@ const INSFooter = () => {
     };
     getSystems();
   }, [FooterData]);
+  */
 
   return (
     <>
