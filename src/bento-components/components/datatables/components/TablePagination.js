2fa96c5f93df9f649a77606a99a01f707871e18f
Subhash Vallala <sgvallala@gmail.com>
2023-04-25 15:09:44 -0400

    INS-644

--- src/bento-components/components/datatables/components/TablePagination.js
+++ src/bento-components/components/datatables/components/TablePagination.js
@@ -69,6 +69,11 @@ class TablePagination extends React.Component {
             page={getPageValue(count, rowsPerPage, page)}
             labelRowsPerPage={textLabels.rowsPerPage}
             labelDisplayedRows={({ from, to, count }) => `${from}-${to} ${textLabels.displayRows} ${count}`}
+            firstIconButtonProps={{
+              id: 'pagination-first',
+              'data-testid': 'pagination-first',
+              'aria-label': textLabels.first,
+            }}
             backIconButtonProps={{
               id: 'pagination-back',
               'data-testid': 'pagination-back',
@@ -78,6 +83,11 @@ class TablePagination extends React.Component {
               id: 'pagination-next',
               'data-testid': 'pagination-next',
               'aria-label': textLabels.next,
+            }}
+            lastIconButtonProps={{
+              id: 'pagination-last',
+              'data-testid': 'pagination-last',
+              'aria-label': textLabels.last,
             }}
             SelectProps={{
               id: 'pagination-input',
