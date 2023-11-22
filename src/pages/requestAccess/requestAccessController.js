872410b31e0ff8c2124e83f3af263e68c0819bb2
Subhash Vallala <sgvallala@gmail.com>
2023-11-22 12:45:54 -0500

    INS-747

--- src/pages/requestAccess/requestAccessController.js
+++ src/pages/requestAccess/requestAccessController.js
@@ -2,19 +2,15 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import { DataAccessRequest } from '@bento-core/data-access-request';
 import { Typography } from '../../components/Wrappers/Wrappers';
-import {DataAccessRequest} from '@bento-core/data-access-request';
-import { formFields, pageTitle, SUBMIT_REQUEST_ACCESS } from '../../bento/requestAccessData';
+import {
+  formFields, pageTitle, SUBMIT_REQUEST_ACCESS, GET_ACCESS_CONTROL_LEVEL_DETAILS_QUERY,
+} from '../../bento/requestAccessData';
 import custodianUtils from '../../utils/custodianUtilFuncs';
 import Stats from '../../components/Stats/AllStatsController';
 import { bentoHelpEmail } from '../../bento/loginData';
-import AlertMessage from "../../bento-core/AlertMessage";
-
-// Importing GraphQL Query.
-import {
-  GET_ACCESS_CONTROL_LEVEL_DETAILS_QUERY,
-} from '../../bento/requestAccessData';
-
+import AlertMessage from '../../bento-core/AlertMessage';
 
 const accessRequestController = ({ match }) => {
   const { loading, error, data } = useQuery(GET_ACCESS_CONTROL_LEVEL_DETAILS_QUERY,
@@ -36,14 +32,14 @@ const accessRequestController = ({ match }) => {
   return (
     <div>
       <Stats />
-      <DataAccessRequest 
-       data= {data} 
-       formFields = {formFields}
-       pageTitle = {pageTitle}
-       SUBMIT_REQUEST_ACCESS ={SUBMIT_REQUEST_ACCESS}
-       custodianUtils={custodianUtils}
-       bentoHelpEmail={bentoHelpEmail}
-       AlertMessage = {AlertMessage}
+      <DataAccessRequest
+        data={data}
+        formFields={formFields}
+        pageTitle={pageTitle}
+        SUBMIT_REQUEST_ACCESS={SUBMIT_REQUEST_ACCESS}
+        custodianUtils={custodianUtils}
+        bentoHelpEmail={bentoHelpEmail}
+        AlertMessage={AlertMessage}
       />
     </div>
   );
