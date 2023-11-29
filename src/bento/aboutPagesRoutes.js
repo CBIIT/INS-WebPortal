10f070b08ed658a8e494dbdb6944da4214f3358a
Subhash Vallala <sgvallala@gmail.com>
2023-11-29 12:30:43 -0500

    INS-795

--- src/bento/aboutPagesRoutes.js
+++ src/bento/aboutPagesRoutes.js
@@ -1,4 +1,4 @@
 export default [
-  '/bento',
+  '/about',
   '/resources',
 ];
--- src/components/NavBar/NavBarView.js
+++ src/components/NavBar/NavBarView.js
@@ -1,40 +1,157 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { NavBar } from '@bento-core/nav-bar';
-import { accessLevelTypes } from '@bento-core/authentication';
-import {
-  navBarData, navBarCartData, navBarstyling,
-} from '../../bento/navigationBarData';
-import Login from '../Auth/loginComponent';
-import { enableAuthentication, PUBLIC_ACCESS } from '../../bento/siteWideConfig';
+import { useLocation, NavLink } from 'react-router-dom';
+import styled from 'styled-components';
 
-const BentoNavBar = ({ cartFieldIds = [] }) => {
-  const isSignedIn = useSelector((state) => state.login.isSignedIn);
-  const { METADATA_ONLY } = accessLevelTypes;
+const Nav = styled.div`
+    top: 0;
+    left: 0;
+    position: fixed;
+    width: 100%;
+    height: 40px;
+    margin-top: 100px;
+    background-color: rgb(62, 60, 63);
+    z-index: 1100;
+`;
 
-  const getNumberOfCase = () => {
-    const { length: numberOfCases } = cartFieldIds;
+const NavContainer = styled.div`
+    margin: 0 auto;
+    width: 100%;
+    text-align: center;
+    position: relative;
+`;
 
-    if (!enableAuthentication || PUBLIC_ACCESS === METADATA_ONLY) return numberOfCases;
+const UlContainer = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+`;
 
-    return isSignedIn ? numberOfCases : 0;
-  };
+const LiSection = styled.li`
+  display: inline-block;
+  position: relative;
+  line-height: 40px;
+  letter-spacing: 1px;
+  text-align: center;
+  color: #ffffff;
+  font-size: 17px;
+  font-weight: 600;
+  font-family: Nunito;
+  text-decoration: none;
+  cursor: pointer;
+  transition:all 0.3s ease-in-out;
 
-  const getLoginComponent = () => {
-    if (!enableAuthentication) return undefined;
-    return Login;
-  };
+  a{
+    display: block;
+    color: #ffffff;
+    text-decoration: none;
+    padding: 0 30px;
+  }
 
+  &:hover {
+    color: #ffffff;
+    background-color: #241F24;
+  }
+
+  .menuLabel {
+    display: block;
+    color: #ffffff;
+    text-decoration: none;
+    padding: 0 30px;
+  }
+
+  &:hover .dropdown-block {
+    display: block;
+  }
+`;
+
+const MenuDropDown = styled.div`
+  width: 233px;
+  padding: 0;
+  background-color: unset;
+  border: 0;
+  position: absolute;
+  display: none;
+  position: absolute;
+  z-index: 999;
+  padding: 0;
+  left: 0;
+  top: 28px;
+
+  &::before {
+    content: '';
+    clip-path: polygon(50% 0, 100% 100%, 0 100%);
+    height: 13px;
+    width: 15px;
+    background-color: #ffffff;
+    left: 50px;
+    position: absolute;
+  }
+
+  .dropdown-list {
+    list-style: none;
+    background-color: #ffffff;
+    margin-top: 12px;
+    padding: 10px 20px;
+    box-shadow: 0px 3px 6px 3px rgb(73 73 73 / 39%);
+  }
+
+  .dropdown-list li {
+    text-align: left;
+  }
+
+  .dropdown-list li a {
+    overflow-wrap: normal;
+    text-decoration: none;
+    color: #413E41;
+    font-size: 16px;
+    line-height: 20px;
+    font-weight: 300;
+    font-family: Nunito;
+    display: block;
+    padding: 10px 0;
+
+    &:hover {
+      color: black;
+      font-weight: 800;
+    }
+  }
+`;
+
+const activeStyle = {
+  color: '#ffffff',
+  backgroundColor: '#241F24',
+};
+
+const subMenuActiveStyle = {
+  color: 'black',
+  fontWeight: '800',
+};
+
+const NavBar = () => {
+  const path = useLocation().pathname;
   return (
-    <>
-      <NavBar
-        navBarData={navBarData}
-        navBarCartData={navBarCartData}
-        navBarstyling={navBarstyling}
-        numberOfCases={getNumberOfCase()}
-        LoginComponent={getLoginComponent()}
-      />
-    </>
+    <Nav>
+      <NavContainer>
+        <UlContainer>
+          <LiSection style={path === '/home' ? activeStyle : null}><NavLink to="/home">Home</NavLink></LiSection>
+          <LiSection style={path === '/explore' ? activeStyle : null}><NavLink to="/explore">Explore</NavLink></LiSection>
+          <LiSection style={path === '/programs' ? activeStyle : null}><NavLink to="/programs">Programs</NavLink></LiSection>
+          <LiSection style={path === '/about' ? activeStyle : null}>
+            <span className="menuLabel">
+              About
+            </span>
+            <MenuDropDown className="dropdown-block">
+              <ul className="dropdown-list">
+                <li><NavLink to="/about" style={path === '/about' ? subMenuActiveStyle : null}>About INS</NavLink></li>
+                <li><a href="/INS_glossary_v1.2.0.pdf" target="_blank" rel="noreferrer">Glossary (PDF)</a></li>
+                <li><a href="/Release_v1.3.0.pdf" target="_blank" rel="noreferrer">Release Notes (PDF)</a></li>
+              </ul>
+            </MenuDropDown>
+          </LiSection>
+        </UlContainer>
+      </NavContainer>
+    </Nav>
   );
 };
-export default BentoNavBar;
+
+export default NavBar;
