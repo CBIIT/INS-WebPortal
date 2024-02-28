c7f984ae20b2563783a41a3734fe418effc9a2f9
Subhash Vallala <sgvallala@gmail.com>
2024-02-28 12:41:24 -0500

    INS-903

--- src/pages/dashTemplate/DashTemplateController.js
+++ src/pages/dashTemplate/DashTemplateController.js
@@ -23,7 +23,395 @@ const getDashData = (states) => {
     return result;
   }
 
-  const [dashData, setDashData] = useState(null);
+  let [dashData] = useState(null);
+  const [setDashData] = useState(null);
+
+  dashData = {
+    numberOfPrograms: 43,
+    numberOfGrants: 2189,
+    numberOfProjects: 588,
+    numberOfPublications: 11873,
+    projectCountByProgram: [
+      {
+        group: 'Moonshot',
+        subjects: 297,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: 'CCDI',
+        subjects: 53,
+        __typename: 'SearchProjectsReturnObject',
+      },
+    ],
+    projectCountByDOC: [
+      {
+        group: 'DCTD',
+        subjects: 112,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: 'DCB',
+        subjects: 96,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: 'DCCPS',
+        subjects: 70,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: 'DCP',
+        subjects: 22,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: 'SBIR',
+        subjects: 16,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: 'OD',
+        subjects: 10,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: 'CCT',
+        subjects: 9,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: 'OCC',
+        subjects: 6,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: 'CCG',
+        subjects: 5,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: 'CRCHD',
+        subjects: 3,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: 'CSSI',
+        subjects: 1,
+        __typename: 'SearchProjectsReturnObject',
+      },
+    ],
+    projectCountByFiscalYear: [
+      {
+        group: '2022',
+        subjects: 135,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: '2021',
+        subjects: 73,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: '2020',
+        subjects: 41,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: '2019',
+        subjects: 37,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: '2018',
+        subjects: 32,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: '2017',
+        subjects: 18,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: '2023',
+        subjects: 14,
+        __typename: 'SearchProjectsReturnObject',
+      },
+    ],
+    projectCountByAwardAmount: [
+      {
+        group: '$2M to $4M',
+        subjects: 91,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: '\u003c$1M',
+        subjects: 84,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: '$4M to $10M',
+        subjects: 74,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: '$1M to $2M',
+        subjects: 72,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: '\u003e$10M',
+        subjects: 29,
+        __typename: 'SearchProjectsReturnObject',
+      },
+    ],
+    publicationCountByYear: [
+      {
+        group: '1609459200000',
+        subjects: 1149,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: '1577836800000',
+        subjects: 996,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: '1640995200000',
+        subjects: 994,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: '1546300800000',
+        subjects: 697,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: '1514764800000',
+        subjects: 535,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: '1672531200000',
+        subjects: 220,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: '1483228800000',
+        subjects: 156,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: '1451606400000',
+        subjects: 1,
+        __typename: 'SearchProjectsReturnObject',
+      },
+    ],
+    publicationCountByRCR: [
+      {
+        group: '',
+        subjects: 1017,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: '2 to 5',
+        subjects: 926,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: '\u003e 5',
+        subjects: 611,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: '1.25 to 2',
+        subjects: 555,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: '0.8 to 1.25',
+        subjects: 484,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: '0.2 to 0.5',
+        subjects: 444,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: '0.5 to 0.8',
+        subjects: 410,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: '\u003c 0.2',
+        subjects: 301,
+        __typename: 'SearchProjectsReturnObject',
+      },
+    ],
+    publicationCountByCitation: [
+      {
+        group: '\u003c\u003d4',
+        subjects: 1780,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: '\u003e\u003d20',
+        subjects: 1475,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: '5 to 9',
+        subjects: 729,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: '10 to 14',
+        subjects: 467,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: '15 to 19',
+        subjects: 297,
+        __typename: 'SearchProjectsReturnObject',
+      },
+    ],
+    filterProjectCountByProgram: [
+      {
+        group: 'Moonshot',
+        subjects: 297,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: 'CCDI',
+        subjects: 53,
+        __typename: 'SearchProjectsReturnObject',
+      },
+    ],
+    filterProjectCountByDOC: [
+      {
+        group: 'DCTD',
+        subjects: 112,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: 'DCB',
+        subjects: 96,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: 'DCCPS',
+        subjects: 70,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: 'DCP',
+        subjects: 22,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: 'SBIR',
+        subjects: 16,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: 'OD',
+        subjects: 10,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: 'CCT',
+        subjects: 9,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: 'OCC',
+        subjects: 6,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: 'CCG',
+        subjects: 5,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: 'CRCHD',
+        subjects: 3,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: 'CSSI',
+        subjects: 1,
+        __typename: 'SearchProjectsReturnObject',
+      },
+    ],
+    filterProjectCountByFiscalYear: [
+      {
+        group: '2022',
+        subjects: 135,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: '2021',
+        subjects: 73,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: '2020',
+        subjects: 41,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: '2019',
+        subjects: 37,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: '2018',
+        subjects: 32,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: '2017',
+        subjects: 18,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: '2023',
+        subjects: 14,
+        __typename: 'SearchProjectsReturnObject',
+      },
+    ],
+    filterProjectCountByAwardAmount: [
+      {
+        group: '$2M to $4M',
+        subjects: 91,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: '\u003c$1M',
+        subjects: 84,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: '$4M to $10M',
+        subjects: 74,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: '$1M to $2M',
+        subjects: 72,
+        __typename: 'SearchProjectsReturnObject',
+      },
+      {
+        group: '\u003e$10M',
+        subjects: 29,
+        __typename: 'SearchProjectsReturnObject',
+      },
+    ],
+    __typename: 'SearchProjectsResult',
+  };
+
+  console.log('dashData: ', dashData);
 
   const activeFilters = {
     ...getFilters(filterState),
