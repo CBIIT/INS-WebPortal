0e7b29904e4e00c925bedca218cdb430fab81a90
Subhash Vallala <sgvallala@gmail.com>
2024-03-08 12:22:09 -0500

    INS-824

--- src/pages/dashTemplate/sideBar/BentoFacetFilterStyle.js
+++ src/pages/dashTemplate/sideBar/BentoFacetFilterStyle.js
@@ -114,10 +114,10 @@ export default () => ({
   searchContainer: {
     paddingTop: '15px',
     margin: '0 2px',
-    marginRight: 6,
+    marginRight: 0,
   },
   findCaseButton: {
-    marginLeft: '105px',
+    marginLeft: 0,
     backgroundColor: '#10A075',
     boxSizing: 'border-box',
     height: 30,
@@ -126,7 +126,7 @@ export default () => ({
     cursor: 'pointer',
     borderRadius: 11,
     display: 'flex',
-    marginRight: 6,
+    marginRight: 0,
     justifyContent: 'center',
     alignItems: 'center',
   },
