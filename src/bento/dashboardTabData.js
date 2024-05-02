d2d21628fcdcaf84e5f34b0e8f896730d8d647e7
Subhash Vallala <sgvallala@gmail.com>
2024-05-02 13:56:13 -0400

    INS-991

--- src/bento/dashboardTabData.js
+++ src/bento/dashboardTabData.js
@@ -326,7 +326,7 @@ export const tabContainers = [
         className: 'programs_cancer_type_str_4',
       },
       {
-        dataField: 'program_acronym',
+        dataField: 'data_link_and_program_acronym',
         header: 'Data Location Details',
         cellType: cellTypes.CONDITIONAL_EXTERNAL_LINK,
         linkAttr: {
