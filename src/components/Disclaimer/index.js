302fd71523f45cd4abeded5b781d088f75701702
Chen <jonkiky@gmail.com>
2024-06-13 09:55:26 -0400

    eslint fix

--- src/components/Disclaimer/index.js
+++ src/components/Disclaimer/index.js
@@ -1,25 +1,27 @@
-/* eslint-disable */
 import React, { useState } from 'react';
 import { withStyles } from '@material-ui/core';
 
-const DisClaimer = ({classes}) => {
-
-  return (
-    <>
-      <div className={classes.disclaimer}>
-        <p>
-          The INS website is under active development. Some NCI-supported projects and associated outputs may not be represented here.
-        </p>
-        <p>
-          Please contact the
-          <a className={classes.aboutLink} href="mailto:nciofficeofdatasharing@mail.nih.gov?Subject=Index%20of%20NCI%20Studies%20feedback">ODS mailbox</a>
-          to provide updates or feedback.
-        </p>
-      </div>
-    </>
-  );
-};
-
+const DisClaimer = ({ classes }) => (
+  <>
+    <div className={classes.disclaimer}>
+      <p>
+        The INS website is under active development.
+        Some NCI-supported projects and associated
+        outputs may not be represented here.
+      </p>
+      <p>
+        Please contact the
+        <a
+          className={classes.aboutLink}
+          href="mailto:nciofficeofdatasharing@mail.nih.gov?Subject=Index%20of%20NCI%20Studies%20feedback"
+        >
+          ODS mailbox
+        </a>
+        to provide updates or feedback.
+      </p>
+    </div>
+  </>
+);
 
 const styles = () => ({
   disclaimer: {
@@ -39,10 +41,10 @@ const styles = () => ({
       lineHeight: '16px',
     },
   },
- aboutLink: {
+  aboutLink: {
     fontWeight: '800',
     color: 'white',
     padding: '5px',
   },
-})
+});
 export default withStyles(styles, { withTheme: true })(DisClaimer);
