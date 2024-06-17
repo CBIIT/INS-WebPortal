0d5790f1173607491530df2ffc2cf71d399e4722
jonkiky <cheny39@nih.gov>
2024-06-17 17:19:49 -0400

    Update src/components/Disclaimer/index.js
    Co-authored-by: codacy-production[bot] <61871480+codacy-production[bot]@users.noreply.github.com>

--- src/components/Disclaimer/index.js
+++ src/components/Disclaimer/index.js
@@ -28,7 +28,7 @@ const styles = () => ({
     position: 'relative',
     backgroundColor: '#b31d3d',
     width: '100%',
-    padding: '10px 0',
+    padding: "10px 0",
     textAlign: 'center',
     color: 'white',
     fontSize: "14px",
