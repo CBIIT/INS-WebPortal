7212f8ccff6d145bec58efa227096cc7e809747a
jonkiky <cheny39@nih.gov>
2024-06-17 17:18:51 -0400

    Update src/components/Disclaimer/index.js
    Co-authored-by: codacy-production[bot] <61871480+codacy-production[bot]@users.noreply.github.com>

--- src/components/Disclaimer/index.js
+++ src/components/Disclaimer/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { withStyles } from '@material-ui/core';
+import { withStyles } from "@material-ui/core";
 
 const DisClaimer = ({ classes }) => (
   <>
