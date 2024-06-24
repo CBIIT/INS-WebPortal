8c345e79936432b2299e5ae10ef7f5ec34512f79
Subhash Vallala <sgvallala@gmail.com>
2024-06-24 14:21:27 -0400

    INS-1060

--- src/bento/globalFooterData.js
+++ src/bento/globalFooterData.js
@@ -33,7 +33,7 @@ export default {
       items: [
         {
           text: 'Release Notes',
-          link: 'Release_v2.1.0.pdf',
+          link: 'Release_v2.2.0.pdf',
         },
         {
           text: `FE Version: ${env.REACT_APP_FE_VERSION || '0.0.0.00'}`,
