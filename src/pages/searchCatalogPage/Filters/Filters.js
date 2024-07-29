e3aac9b7520ed237e17e49ba862e61a6a5a81344
Subhash Vallala <sgvallala@gmail.com>
2024-07-29 13:20:05 -0400

    INS-1107

--- src/pages/searchCatalogPage/Filters/Filters.js
+++ src/pages/searchCatalogPage/Filters/Filters.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import React, { useEffect } from 'react';
 import {
   useLocation,
--- src/pages/searchCatalogPage/SearchCatalogPage.js
+++ src/pages/searchCatalogPage/SearchCatalogPage.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import React, { useState, useEffect } from 'react';
 import {
   useLocation,
