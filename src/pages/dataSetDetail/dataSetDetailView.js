d238168be547a28201a62b6b202ff7699aca8a24
Chen <jonkiky@gmail.com>
2024-10-18 01:02:02 -0400

    update the read more feature

--- src/pages/dataSetDetail/dataSetDetailView.js
+++ src/pages/dataSetDetail/dataSetDetailView.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Container,
   Grid,
@@ -17,223 +17,248 @@ import {
 
 const DataSetDetailView = ({
   classes, data,
-}) => (
-  <Container className={classes.mainContainer}>
-    <Grid container spacing={2} alignItems="center" justify="space-between" className={classes.nav}>
-      <Grid item>
-        <Link href="#datasets" className={classes.navLink}>
-          Explore Dataset
-        </Link>
-        {'    '}
-        >
-        {'    '}
-        {data.dataset_title || ''}
+}) => {
+  const descMaxLength = 950;
+  const [expanded, setExpanded] = useState(false);
+
+  // Function to toggle "Read More" and "Read Less"
+  const toggleExpand = () => {
+    setExpanded(!expanded);
+  };
+
+  // Truncate description to 948 characters if not expanded
+  const truncatedDescription = data.description && data.description.length > descMaxLength
+    ? `${data.description.substring(0, descMaxLength)}...`
+    : data.description;
+
+  return (
+    <Container className={classes.mainContainer}>
+      <Grid container spacing={2} alignItems="center" justify="space-between" className={classes.nav}>
+        <Grid item>
+          <Link href="#datasets" className={classes.navLink}>
+            Explore Dataset
+          </Link>
+          {'    '}
+          >
+          {'    '}
+          {data.dataset_title || ''}
+        </Grid>
       </Grid>
-    </Grid>
-    <div className={classes.container}>
-      <div className={classes.innerContainer}>
-        <div className={classes.header}>
-          <div className={classes.logo}>
-            <img
-              src={icon}
-              alt="dataset header icon"
-            />
-          </div>
-          <div className={classes.headerTitle}>
-            <div className={classes.headerMainTitle} id="program_detail_title">
-              <span>
-                Dataset:
-                {data.dataset_title || ''}
-              </span>
+      <div className={classes.container}>
+        <div className={classes.innerContainer}>
+          <div className={classes.header}>
+            <div className={classes.logo}>
+              <img
+                src={icon}
+                alt="dataset header icon"
+              />
             </div>
-            <div className={cn(classes.headerMSubTitle, classes.headerSubTitleCate)}>
-              <span id="program_detail_subtile">
-                dbGaP:
-                {' '}
-                {data.dbGaP_phs || ''}
-              </span>
+            <div className={classes.headerTitle}>
+              <div className={classes.headerMainTitle} id="program_detail_title">
+                <span>
+                  Dataset:
+                  {data.dataset_title || ''}
+                </span>
+              </div>
+              <div className={cn(classes.headerMSubTitle,
+                classes.headerSubTitleCate, classes.link)}
+              >
+                <span id="program_detail_subtile" className={classes.link}>
+                  dbGaP:
+                  {' '}
+                  {data.dbGaP_phs || ''}
+                </span>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    <div className={classes.studyContainer}>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <Typography variant="h6" component="h2" className={classes.studyHeader}>
-            Study Description
-          </Typography>
-          <Typography variant="body1" paragraph className={classes.studyContent}>
-            {data.description || ''}
-            {' '}
-            <Link href="#datasets" color="primary">
-              Read More
-            </Link>
-          </Typography>
-
-        </Grid>
-      </Grid>
-    </div>
-    <div className={classes.basicInformationContainer}>
-      <Grid container spacing={4}>
-        {/* Basic Information */}
-        <Grid item xs={12} md={4} className={cn(classes.space, classes.borderRight)}>
-          <Typography variant="h2" className={classes.title}>Basic Information</Typography>
-          <div className={classes.subSection}>
-            <Typography variant="body2" className={classes.subTitle}>
-              <strong>Study Page </strong>
+      <div className={classes.studyContainer}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Typography variant="h6" component="h2" className={classes.studyHeader}>
+              Study Description
             </Typography>
-            <Typography variant="body2" className={classes.text}>
-              <Link href={data.dbGaP_URL} target="_blank">
-                dbGaP:
-                {data.dbGaP_phs || ''}
-                <img
-                  src={externalLinkIcon.src}
-                  alt={externalLinkIcon.alt}
-                  className={classes.externalLinkIcon}
-                />
-              </Link>
-
-            </Typography>
-            <Typography variant="body2" className={classes.subTitle}>
-              <strong>NCI Division/Office/Center (DOC): </strong>
-            </Typography>
-            <Typography variant="body2" className={classes.text}>
-              {data.dataset_doc || ''}
-            </Typography>
-            <Typography variant="body2" className={classes.subTitle}>
-              <strong>Release Date: </strong>
-            </Typography>
-            <Typography variant="body2" className={classes.text}>
-              {data.release_date || ''}
-            </Typography>
-            <Typography variant="body2" className={classes.subTitle}>
-              <strong>Principal Investigator(s): </strong>
-            </Typography>
-            <Typography variant="body2" className={classes.text}>
-              {data.PI_name || ''}
-            </Typography>
-            <Typography variant="body2" className={classes.subTitle}>
-              <strong>Funding Source(s): </strong>
-            </Typography>
-            <Typography variant="body2" className={classes.text}>
-              {data.funding_source || ''}
-            </Typography>
-            <Typography variant="body2" className={classes.subTitle}>
-              <strong>Cited Publication PMID(s): </strong>
-            </Typography>
-            <Typography variant="body2" className={classes.text}>
-              {data.dataset_pmid || ''}
-            </Typography>
-          </div>
-        </Grid>
-        <div className={classes.divder} />
-        <Grid item xs={12} md={8} className={classes.space}>
-          <Typography variant="h6" className={classes.title}>Data Details</Typography>
-          <Grid container className={classes.subSection}>
-            <Grid item xs={12} md={6}>
-              <Typography variant="body2" className={classes.subTitle}>
-                <strong>Study Type </strong>
-              </Typography>
-              <Typography variant="body2" className={classes.text}>
-                {data.study_type || ''}
-              </Typography>
-
-              <Typography variant="body2" className={classes.subTitle}>
-                <strong>Limitations for Reuse </strong>
-              </Typography>
-              <Typography variant="body2" className={classes.text}>
-                {data.limitations_for_reuse || ''}
-              </Typography>
-              <Typography variant="body2" className={classes.subTitle}>
-                <strong>Assay Method </strong>
-              </Typography>
-              <Typography variant="body2" className={classes.text}>
-                {data.assay_method || ''}
-              </Typography>
-              <Typography variant="body2" className={classes.subTitle}>
-                <strong>Participant Count; Sample Count </strong>
-              </Typography>
-              <Typography variant="body2" className={classes.text}>
-                {data.participant_count || ''}
-              </Typography>
-
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <Typography variant="body2" className={classes.subTitle}>
-                <strong>Primary Disease </strong>
-              </Typography>
-              <Typography variant="body2" className={classes.text}>
-                {data.primary_disease || ''}
-              </Typography>
-              <Typography variant="body2" className={classes.subTitle}>
-                <strong>Related Genes </strong>
-              </Typography>
-              <Typography variant="body2" className={classes.text}>
-                {data.related_genes || ''}
-              </Typography>
-              <Typography variant="body2" className={classes.subTitle}>
-                <strong>Related Diseases </strong>
-              </Typography>
-              <Typography variant="body2" className={classes.text}>
-                {data.related_diseases || ''}
-              </Typography>
-              <Typography variant="body2" className={classes.subTitle}>
-                <strong>Related Terms </strong>
-              </Typography>
-              <Typography variant="body2" className={classes.text}>
-                {data.related_terms || ''}
-              </Typography>
-
-              <Typography variant="body2" className={classes.subTitle}>
-                <strong>Study Link(s) </strong>
-              </Typography>
-              {data.study_links && data.study_links.length > 0 ? (
-                data.study_links.map((link, index) => (
-                  <Typography variant="body2" className={classes.text} key={index}>
-                    <Link href={link} target="_blank">
-                      {link}
-                    </Link>
-                  </Typography>
-                ))
-              ) : (
-                ''
+            <Typography variant="body1" paragraph className={classes.studyContent}>
+              {expanded ? data.description : truncatedDescription || ''}
+              {' '}
+              {data.description && data.description.length > descMaxLength && (
+              <Button onClick={toggleExpand} color="primary" className={classes.link}>
+                {expanded ? '' : 'Read More'}
+              </Button>
               )}
+            </Typography>
+
+          </Grid>
+        </Grid>
+      </div>
+      <div className={classes.basicInformationContainer}>
+        <Grid container spacing={4}>
+          {/* Basic Information */}
+          <Grid item xs={12} md={4} className={cn(classes.space, classes.borderRight)}>
+            <Typography variant="h2" className={classes.title}>Basic Information</Typography>
+            <div className={classes.subSection}>
+              <Typography variant="body2" className={classes.subTitle}>
+                <strong>Study Page </strong>
+              </Typography>
+              <Typography variant="body2" className={classes.text}>
+                <Link href={data.dbGaP_URL} target="_blank" className={classes.link}>
+                  dbGaP:
+                  {data.dbGaP_phs || ''}
+                  <img
+                    src={externalLinkIcon.src}
+                    alt={externalLinkIcon.alt}
+                    className={classes.externalLinkIcon}
+                  />
+                </Link>
+
+              </Typography>
+              <Typography variant="body2" className={classes.subTitle}>
+                <strong>NCI Division/Office/Center (DOC): </strong>
+              </Typography>
+              <Typography variant="body2" className={classes.text}>
+                {data.dataset_doc || ''}
+              </Typography>
+              <Typography variant="body2" className={classes.subTitle}>
+                <strong>Release Date: </strong>
+              </Typography>
+              <Typography variant="body2" className={classes.text}>
+                {data.release_date || ''}
+              </Typography>
+              <Typography variant="body2" className={classes.subTitle}>
+                <strong>Principal Investigator(s): </strong>
+              </Typography>
+              <Typography variant="body2" className={classes.text}>
+                {data.PI_name || ''}
+              </Typography>
+              <Typography variant="body2" className={classes.subTitle}>
+                <strong>Funding Source(s): </strong>
+              </Typography>
+              <Typography variant="body2" className={classes.text}>
+                {data.funding_source || ''}
+              </Typography>
+              <Typography variant="body2" className={classes.subTitle}>
+                <strong>Cited Publication PMID(s): </strong>
+              </Typography>
+              <Typography variant="body2" className={classes.text}>
+                {data.dataset_pmid || ''}
+              </Typography>
+            </div>
+          </Grid>
+          <div className={classes.divder} />
+          <Grid item xs={12} md={8} className={classes.space}>
+            <Typography variant="h6" className={classes.title}>Data Details</Typography>
+            <Grid container className={classes.subSection}>
+              <Grid item xs={12} md={6}>
+                <Typography variant="body2" className={classes.subTitle}>
+                  <strong>Study Type </strong>
+                </Typography>
+                <Typography variant="body2" className={classes.text}>
+                  {data.study_type || ''}
+                </Typography>
+
+                <Typography variant="body2" className={classes.subTitle}>
+                  <strong>Limitations for Reuse </strong>
+                </Typography>
+                <Typography variant="body2" className={classes.text}>
+                  {data.limitations_for_reuse || ''}
+                </Typography>
+                <Typography variant="body2" className={classes.subTitle}>
+                  <strong>Assay Method </strong>
+                </Typography>
+                <Typography variant="body2" className={classes.text}>
+                  {data.assay_method || ''}
+                </Typography>
+                <Typography variant="body2" className={classes.subTitle}>
+                  <strong>Participant Count; Sample Count </strong>
+                </Typography>
+                <Typography variant="body2" className={classes.text}>
+                  {data.participant_count || ''}
+                </Typography>
+
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <Typography variant="body2" className={classes.subTitle}>
+                  <strong>Primary Disease </strong>
+                </Typography>
+                <Typography variant="body2" className={classes.text}>
+                  {data.primary_disease || ''}
+                </Typography>
+                <Typography variant="body2" className={classes.subTitle}>
+                  <strong>Related Genes </strong>
+                </Typography>
+                <Typography variant="body2" className={classes.text}>
+                  {data.related_genes || ''}
+                </Typography>
+                <Typography variant="body2" className={classes.subTitle}>
+                  <strong>Related Diseases </strong>
+                </Typography>
+                <Typography variant="body2" className={classes.text}>
+                  {data.related_diseases || ''}
+                </Typography>
+                <Typography variant="body2" className={classes.subTitle}>
+                  <strong>Related Terms </strong>
+                </Typography>
+                <Typography variant="body2" className={classes.text}>
+                  {data.related_terms || ''}
+                </Typography>
+
+                <Typography variant="body2" className={classes.subTitle}>
+                  <strong>Study Link(s) </strong>
+                </Typography>
+                {data.study_links && data.study_links.length > 0 ? (
+                  data.study_links.map((link, index) => (
+                    <Typography variant="body2" className={classes.text} key={index}>
+                      <Link href={link} target="_blank" className={classes.link}>
+                        {link}
+                        <img
+                          src={externalLinkIcon.src}
+                          alt={externalLinkIcon.alt}
+                          className={classes.externalLinkIcon}
+                        />
+                      </Link>
+                    </Typography>
+                  ))
+                ) : (
+                  ''
+                )}
+              </Grid>
             </Grid>
           </Grid>
         </Grid>
-      </Grid>
-    </div>
-    <div className={classes.additionalContainer}>
-      <Grid container spacing={2}>
-        <Grid item xs={12} className={classes.space}>
-          <Typography variant="h6" className={classes.title}>Additional Details Coming Soon</Typography>
-          <div className={classes.subSection}>
-            <Typography variant="body2" className={classes.subTitle}>
-              <strong>Participant/Sample Details </strong>
-            </Typography>
-            <Typography variant="body2" className={classes.text}>
-              This information is coming soon.
-            </Typography>
+      </div>
+      <div className={classes.additionalContainer}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} className={classes.space}>
+            <Typography variant="h6" className={classes.title}>Additional Details Coming Soon</Typography>
+            <div className={classes.subSection}>
+              <Typography variant="body2" className={classes.subTitle}>
+                <strong>Participant/Sample Details </strong>
+              </Typography>
+              <Typography variant="body2" className={classes.text}>
+                This information is coming soon.
+              </Typography>
 
-            <Typography variant="body2" className={classes.subTitle}>
-              <strong>Data Details </strong>
-            </Typography>
-            <Typography variant="body2" className={classes.text}>
-              This information is coming soon.
-            </Typography>
-            <Typography variant="body2" className={classes.subTitle}>
-              <strong>Program(s) </strong>
-            </Typography>
-            <Typography variant="body2" className={classes.text}>
-              This information is coming soon.
-            </Typography>
-          </div>
+              <Typography variant="body2" className={classes.subTitle}>
+                <strong>Data Details </strong>
+              </Typography>
+              <Typography variant="body2" className={classes.text}>
+                This information is coming soon.
+              </Typography>
+              <Typography variant="body2" className={classes.subTitle}>
+                <strong>Program(s) </strong>
+              </Typography>
+              <Typography variant="body2" className={classes.text}>
+                This information is coming soon.
+              </Typography>
+            </div>
+          </Grid>
         </Grid>
-      </Grid>
-    </div>
-  </Container>
-);
+      </div>
+    </Container>
+  );
+};
+
 const styles = (theme) => ({
   mainContainer: {
     paddingTop: '10px',
@@ -254,6 +279,9 @@ const styles = (theme) => ({
   },
   navLink: {
     color: '#005EA2',
+  },
+  link: {
+    color: '#571AFF',
   },
   container: {
     paddingTop: '30px',
@@ -414,6 +442,7 @@ const styles = (theme) => ({
     padding: ' 0 32px !important',
     margin: '16px 0',
   },
+
 });
 
 export default withStyles(styles, { withTheme: true })(DataSetDetailView);
