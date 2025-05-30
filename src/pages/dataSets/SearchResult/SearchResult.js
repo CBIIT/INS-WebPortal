771928f8da0fe53c3d6dfa1c100bf88e1ab929d5
Subhash Vallala <sgvallala@gmail.com>
2025-05-30 12:58:50 -0400

    INS-1399

--- src/pages/dataSets/SearchResult/SearchResult.js
+++ src/pages/dataSets/SearchResult/SearchResult.js
@@ -521,7 +521,7 @@ const SearchResult = ({
             const additionalMatches = [];
 
             const hideContent = [
-              { 'Study Page': rst.content.dataset_source_url },
+              { 'study page': rst.content.dataset_source_url },
               { 'PI name': rst.content.PI_name },
               { 'dataset pmid': rst.content.dataset_pmid },
               { 'funding source': rst.content.funding_source },
