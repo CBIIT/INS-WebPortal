433c19896b4d537874bbf947bf4741d24e8b25d5
Subhash Vallala <sgvallala@gmail.com>
2025-05-30 14:10:21 -0400

    INS-1399

--- src/pages/dataSets/SearchResult/SearchResult.js
+++ src/pages/dataSets/SearchResult/SearchResult.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import React, { useEffect } from 'react';
 import {
   useLocation,
@@ -624,7 +625,6 @@ const SearchResult = ({
                     </div>
                   )
                 }
-
                 {
                   additionalMatches.length > 0 && additionalMatches.map((match, index) => (
                     <div className="row align-items-start bodyRow" key={index}>
