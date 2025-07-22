757a4685c3333f3548362cf9a58791def4566d25
Subhash Vallala <sgvallala@gmail.com>
2025-07-22 12:08:35 -0400

    INS-1436

--- src/components/Layout/LayoutView.js
+++ src/components/Layout/LayoutView.js
@@ -35,7 +35,7 @@ const Layout = ({ classes, isSidebarOpened }) => {
       <HashRouter>
         <>
           <Header />
-          <NavBar />
+          {/* <NavBar /> */}
           <Disclaimer />
           <ScreenSizePopup />
           <div
