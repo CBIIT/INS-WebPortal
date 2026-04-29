da7ee285f9589b0becd70c61d3cfca9793c4a3c0
Alec M. <alec.mattu@nih.gov>
2026-04-29 10:29:51 -0400

    feat: Migrate local setup to env file

--- public/injectEnv.js
+++ public/injectEnv.js
@@ -1,22 +1,18 @@
 window.injectedEnv = {
-  REACT_APP_REST_BACKEND_API: 'https://studycatalog-dev.cancer.gov/service',
-  REACT_APP_BACKEND_API: 'https://studycatalog-dev.cancer.gov/v1/graphql/',
-  REACT_APP_BACKEND_PUBLIC_API: 'https://studycatalog-dev.cancer.gov/v1/graphql/',
-  REACT_APP_FILE_SERVICE_API: 'http://localhost:3000/api/files/',
-  REACT_APP_AUTH_SERVICE_API: 'http://localhost:3000/api/auth/',
-  REACT_APP_USER_SERVICE_API: 'http://localhost:3000/api/users/',
+  REACT_APP_REST_BACKEND_API: '',
+  REACT_APP_BACKEND_API: '',
+  REACT_APP_BACKEND_PUBLIC_API: '',
+  REACT_APP_FILE_SERVICE_API: '',
+  REACT_APP_AUTH_SERVICE_API: '',
+  REACT_APP_USER_SERVICE_API: '',
   REACT_APP_DATA_VERSION: '',
   REACT_APP_BE_VERSION: '',
   REACT_APP_FE_VERSION: '',
-  // IDP Auth url
-  REACT_APP_NIH_AUTH_URL: 'https://stsstg.nih.gov/auth/oauth/v2/authorize',
-  // Client IDs for IDP
-  REACT_APP_GOOGLE_CLIENT_ID: 'Sample Id',
-  REACT_APP_NIH_CLIENT_ID: 'Sample Id',
-
-  // Access control settings
-  REACT_APP_AUTH: true,
-  PUBLIC_ACCESS: 'Metadata Only',
-  NODE_LEVEL_ACCESS: true,
-  NODE_LABEL: 'Study Arm(s)',
+  REACT_APP_NIH_AUTH_URL: '',
+  REACT_APP_GOOGLE_CLIENT_ID: '',
+  REACT_APP_NIH_CLIENT_ID: '',
+  REACT_APP_AUTH: '',
+  PUBLIC_ACCESS: '',
+  NODE_LEVEL_ACCESS: '',
+  NODE_LABEL: '',
 };
