794bb8e015f5750104b499f1839a9104abed07ec
Alec M. <alec.mattu@nih.gov>
2026-04-29 10:20:36 -0400

    refactor: Remove unused environment variables from injectEnv.js

--- public/injectEnv.js
+++ public/injectEnv.js
@@ -1,5 +1,4 @@
 window.injectedEnv = {
-  REACT_APP_APPLICATION_VERSION: 'YYYY_MM_DD/HH:MM',
   REACT_APP_REST_BACKEND_API: 'https://studycatalog-dev.cancer.gov/service',
   REACT_APP_BACKEND_API: 'https://studycatalog-dev.cancer.gov/v1/graphql/',
   REACT_APP_BACKEND_PUBLIC_API: 'https://studycatalog-dev.cancer.gov/v1/graphql/',
@@ -20,9 +19,4 @@ window.injectedEnv = {
   PUBLIC_ACCESS: 'Metadata Only',
   NODE_LEVEL_ACCESS: true,
   NODE_LABEL: 'Study Arm(s)',
-
-  // No Longer Used.
-  REACT_APP_BACKEND_GETUSERINFO_API: 'https://k9dc.essential-dev.com/fence/login/',
-  REACT_APP_LOGIN_URL: 'https://nci-crdc-staging.datacommons.io/user/oauth2/authorize?client_id=82pslYFJqA7auRvKYfTOK67jzQAMb8f6C33tlmZz&response_type=code&redirect_uri=https%3A%2F%2Fk9dc.essential-dev.com%2F&scope=openid%20user',
-  REACT_APP_USER_LOGOUT_URL: 'https://k9dc.essential-dev.com/fence/logout',
 };
